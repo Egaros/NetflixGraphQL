0ccb9a025a8c568f884825192557c7cac3827960
Jhericoh Janquill Lumaque <jjnlumaque@iskolarngbayan.pup.edu.ph>
2021-04-08 20:23:24 +0800

    implemented graphql foundation

---
+++ Database/Interfaces/IAllShows.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NetflixGraphQL.Models;
+
+namespace NetflixGraphQL.Database.Interfaces
+{
+    public interface IAllShows
+    {
+        Task<List<MovieModel>> GetAllShows();
+    }
+}
---
+++ Schema/ShowsSchema.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL;
+using NetflixGraphQL.Queries;
+
+namespace NetflixGraphQL.Schema
+{
+    public class ShowsSchema : GraphQL.Types.Schema
+    {
+        public ShowsSchema(IDependencyResolver resolver) : base(resolver)
+        {
+            Query = resolver.Resolve<ShowQueries>();
+        }
+    }
+}
